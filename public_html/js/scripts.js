9943590de315410acf04498366e04b3bdf0fadb2
MichielGeyskens <michielgeyskens@hotmail.be>
2021-10-22 15:35:18 +0200

    Modal open scouts gebouw

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -30,13 +30,21 @@
 //    document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
+    // const mainNav = document.body.querySelector('#mainNav');
+    // if (mainNav) {
+    //     new bootstrap.ScrollSpy(document.body, {
+    //         target: '#mainNav',
+    //         offset: 74,
+    //     });
+    // };
+
+   
+    setTimeout(function () {
+        var myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+        myModal.toggle()
+    }, 100);
+    
+
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
@@ -51,4 +59,4 @@
         });
     });
 
-});
+// });
